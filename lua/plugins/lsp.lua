13743ea960cba17017dbaf6bf99ff4d898d9312d
Colton Donnelly <colton@donn.io>
2023-11-25 11:55:09 -0500

    lua is hard

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -8,26 +8,19 @@ local M = {
 	},
 }
 
+local lspConfigs = {
+	lua_ls = {
+		cmd = { vim.g.nixtools['lua-language-server'] },
+	},
+	nixd = {
+		cmd = { vim.g.nixtools['nixd'] }
+	},
+}
 
 function M.config()
-	local lspConfigs = {
-		lua_ls = {
-			cmd = { vim.g.nixtools['lua-language-server'] },
-		},
-		nixd = {
-			cmd = { vim.g.nixtools['nixd'] }
-		},
-	}
-
 	local lsp = require('lspconfig')
-	-- local lspConfigs = M.lspConfigs
-	-- vim.print(vim.g.nixtools['nixd'])
-	vim.print('initing ' .. #lspConfigs .. ' lsp servers')
-	-- vim.print(vim.base64.encode(vim.inspect(M)))
-	for lsName, lsConfig in ipairs(lspConfigs) do
+	for lsName, lsConfig in pairs(lspConfigs) do
 		lsConfig.capabilities = vim.g.lsp_capabilities
-		vim.print('initing ' .. lsName .. ' with ' .. #lsConfig .. ' opts')
-
 		lsp[lsName].setup(lsConfig)
 	end
 end
