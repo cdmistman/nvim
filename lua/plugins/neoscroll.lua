c998ee136be69c98b66dd538f4e4b61d9fbe1f74
Colton Donnelly <colton@donn.io>
2023-11-27 01:13:42 -0500

    neoscroll

---
+++ lua/plugins/neoscroll.lua
@@ -0,0 +1,12 @@
+return {
+	name = 'neoscroll.nvim',
+	dir = vim.g.nixplugins['neoscroll.nvim'],
+	event = 'VeryLazy',
+
+	config = true,
+	opts = {
+		mappings = { 'zt', 'zz', 'zb' },
+		pre_hook = function() vim.print('zzzcrolling') end,
+	},
+}
+
